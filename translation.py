05843cff4d83a1144cb4b0f05016814fc039c9ef
Cade Jacobson <cade.jacobson@mines.sdsmt.edu>
2023-10-25 08:35:41 -0600

    Create a working German model

--- translation.py
+++ translation.py
@@ -3,11 +3,26 @@ import re
 from unicodedata import normalize
 from pickle import dump
 from numpy import array
-
-def main():
-    print("Hello world")
-
-main()
+from pickle import load
+from numpy import array
+from numpy import argmax
+from keras.preprocessing.text import Tokenizer
+from keras.preprocessing.sequence import pad_sequences
+from keras.models import load_model
+from numpy import array
+from keras.preprocessing.text import Tokenizer
+from keras.preprocessing.sequence import pad_sequences
+from keras.utils import to_categorical
+from keras.models import Sequential
+from keras.layers import LSTM
+from keras.layers import Dense
+from keras.layers import Embedding
+from keras.layers import RepeatVector
+from keras.layers import TimeDistributed
+from keras.callbacks import ModelCheckpoint
+from numpy.random import rand
+from numpy.random import shuffle
+import tensorflow as tf
 
 def load_doc(filename):
     file = open(filename, mode='rt', encoding='utf-8')
@@ -44,8 +59,9 @@ def save_clean_data(sentences, filename):
     dump(sentences, open(filename, 'wb'))
     print('Saved: %s' % filename)
 
+
 # load dataset
-filename = './languages/isl.txt'
+filename = './languages/deu.txt'
 doc = load_doc(filename)
 
 pairs = to_pairs(doc)
@@ -56,3 +72,188 @@ save_clean_data(clean_pairs, 'english-german.pkl')
 
 for i in range(100):
     print('[%s] => [%s]' % (clean_pairs[i,0], clean_pairs[i,1]))
+
+
+def load_clean_sentences(filename):
+    return load(open(filename, 'rb'))
+
+
+# save a list of clean sentences to file
+def save_clean_data(sentences, filename):
+    dump(sentences, open(filename, 'wb'))
+    print('Saved: %s' % filename)
+
+
+# load dataset
+raw_dataset = load_clean_sentences('english-german.pkl')
+
+# reduce dataset size
+n_sentences = 10000
+dataset = raw_dataset[:n_sentences, :]
+# random shuffle
+shuffle(dataset)
+# split into train/test
+train, test = dataset[:9000], dataset[9000:]
+# save
+save_clean_data(dataset, 'english-german-both.pkl')
+save_clean_data(train, 'english-german-train.pkl')
+save_clean_data(test, 'english-german-test.pkl')
+
+
+# load a clean dataset
+def load_clean_sentences(filename):
+    return load(open(filename, 'rb'))
+
+# fit a tokenizer
+def create_tokenizer(lines):
+    tokenizer = Tokenizer()
+    tokenizer.fit_on_texts(lines)
+    return tokenizer
+
+# max sentence length
+def max_length(lines):
+    return max(len(line.split()) for line in lines)
+
+
+# encode and pad sequences
+def encode_sequences(tokenizer, length, lines):
+    # integer encode sequences
+    X = tokenizer.texts_to_sequences(lines)
+    # pad sequences with 0 values
+    X = pad_sequences(X, maxlen=length, padding='post')
+    return X
+
+
+# one hot encode target sequence
+def encode_output(sequences, vocab_size):
+    ylist = list()
+    for sequence in sequences:
+        encoded = to_categorical(sequence, num_classes=vocab_size)
+        ylist.append(encoded)
+    y = array(ylist)
+    y = y.reshape(sequences.shape[0], sequences.shape[1], vocab_size)
+    return y
+
+
+# define NMT model
+def define_model(src_vocab, tar_vocab, src_timesteps, tar_timesteps, n_units):
+    model = Sequential()
+    model.add(Embedding(src_vocab, n_units, input_length=src_timesteps, mask_zero=True))
+    model.add(LSTM(n_units))
+    model.add(RepeatVector(tar_timesteps))
+    model.add(LSTM(n_units, return_sequences=True))
+    model.add(TimeDistributed(Dense(tar_vocab, activation='softmax')))
+    return model
+
+# load datasets
+dataset = load_clean_sentences('english-german-both.pkl')
+train = load_clean_sentences('english-german-train.pkl')
+test = load_clean_sentences('english-german-test.pkl')
+
+# prepare english tokenizer
+eng_tokenizer = create_tokenizer(dataset[:, 0])
+eng_vocab_size = len(eng_tokenizer.word_index) + 1
+eng_length = max_length(dataset[:, 0])
+print('English Vocabulary Size: %d' % eng_vocab_size)
+print('English Max Length: %d' % (eng_length))
+# prepare german tokenizer
+ger_tokenizer = create_tokenizer(dataset[:, 1])
+ger_vocab_size = len(ger_tokenizer.word_index) + 1
+ger_length = max_length(dataset[:, 1])
+print('German Vocabulary Size: %d' % ger_vocab_size)
+print('German Max Length: %d' % (ger_length))
+
+# prepare training data
+trainX = encode_sequences(ger_tokenizer, ger_length, train[:, 1])
+trainY = encode_sequences(eng_tokenizer, eng_length, train[:, 0])
+trainY = encode_output(trainY, eng_vocab_size)
+# prepare validation data
+testX = encode_sequences(ger_tokenizer, ger_length, test[:, 1])
+testY = encode_sequences(eng_tokenizer, eng_length, test[:, 0])
+testY = encode_output(testY, eng_vocab_size)
+
+# define model
+model = define_model(ger_vocab_size, eng_vocab_size, ger_length, eng_length, 256)
+model.compile(loss='categorical_crossentropy', optimizer="adam" )
+# summarize defined model
+print(model.summary())
+
+# fit model
+filename = './model.h5'
+model.fit(trainX, trainY, epochs=64, batch_size=256, validation_data=(testX, testY), verbose=2)
+model.save(filename)
+
+def max_length(lines):
+    return max(len(line.split()) for line in lines)
+
+
+# encode and pad sequences
+def encode_sequences(tokenizer, length, lines):
+    # integer encode sequences
+    X = tokenizer.texts_to_sequences(lines)
+    # pad sequences with 0 values
+    X = pad_sequences(X, maxlen=length, padding='post')
+    return X
+
+
+# map an integer to a word
+def word_for_id(integer, tokenizer):
+    for word, index in tokenizer.word_index.items():
+        if index == integer:
+            return word
+    return None
+
+
+# generate target given source sequence
+def predict_sequence(model, tokenizer, source):
+    prediction = model.predict(source, verbose=0)[0]
+    integers = [argmax(vector) for vector in prediction]
+    target = list()
+    for i in integers:
+        word = word_for_id(i, tokenizer)
+        if word is None:
+            break
+        target.append(word)
+    return ' '.join(target)
+
+
+# evaluate the skill of the model
+def evaluate_model(model, tokenizer, sources, raw_dataset):
+    actual, predicted = list(), list()
+    for i, source in enumerate(sources):
+        # translate encoded source text
+        source = source.reshape((1, source.shape[0]))
+        translation = predict_sequence(model, eng_tokenizer, source)
+        raw_target= raw_dataset[i,0]
+        raw_src = raw_dataset[i,1]
+        if i < 10:
+            print('src=[%s], target=[%s], predicted=[%s]' % (raw_src, raw_target, translation))
+        actual.append([raw_target.split()])
+        predicted.append(translation.split())
+
+
+# load datasets
+dataset = load_clean_sentences('english-german-both.pkl')
+train = load_clean_sentences('english-german-train.pkl')
+test = load_clean_sentences('english-german-test.pkl')
+# prepare english tokenizer
+eng_tokenizer = create_tokenizer(dataset[:, 0])
+eng_vocab_size = len(eng_tokenizer.word_index) + 1
+eng_length = max_length(dataset[:, 0])
+# prepare german tokenizer
+ger_tokenizer = create_tokenizer(dataset[:, 1])
+ger_vocab_size = len(ger_tokenizer.word_index) + 1
+ger_length = max_length(dataset[:, 1])
+# prepare data
+trainX = encode_sequences(ger_tokenizer, ger_length, train[:, 1])
+testX = encode_sequences(ger_tokenizer, ger_length, test[:, 1])
+
+
+model = load_model(filename)
+# test on some training sequences
+print('train')
+print(trainX.shape)
+evaluate_model(model, eng_tokenizer, trainX, train)
+# test on some test sequences
+print('test')
+evaluate_model(model, eng_tokenizer, testX, test)
